d495ea97637540ab489867434f4680cbc6f64947
Bogdan Iulian <vali_0894@yahoo.com>
2022-08-05 17:49:06 +0300

    [US#5] Create rest client

--- src/main/java/com/coffeeshop/rest/RestController.java
+++ src/main/java/com/coffeeshop/rest/RestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin
-@org.springframework.web.bind.annotation.RestController
+@org.springframework.web.bind.annotation.RestController("restController")
 @RequestMapping({"orders/pay"})
 public class RestController {
 
