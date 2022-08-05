487c334c339772634688e6c5ff4aa868f0061f5c
Bogdan Iulian <vali_0894@yahoo.com>
2022-08-05 17:52:21 +0300

    [US#5] Update rest controller

--- src/main/java/com/coffeeshop/rest/RestController.java
+++ src/main/java/com/coffeeshop/rest/RestController.java
@@ -3,6 +3,7 @@ package com.coffeeshop.rest;
 import com.coffeeshop.models.customer.Payment;
 import com.coffeeshop.repository.interfaces.IPaymentRepository;
 import com.coffeeshop.repository.implementations.PaymentRepository;
+import com.coffeeshop.service.implementations.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +16,22 @@ import java.util.List;
 @RequestMapping({"orders/pay"})
 public class RestController {
 
-   private final IPaymentRepository payRepository;
+   private final PaymentService paymentService;
 
    @Autowired
-   public RestController(PaymentRepository payRepository){
-      this.payRepository = payRepository;
+   public RestController(PaymentService paymentService){
+      this.paymentService = paymentService;
    }
 
    @RequestMapping(method = RequestMethod.POST)
-   public Payment create(@RequestBody Payment payToCreate){
-      return payRepository.save(payToCreate);
+   public Payment create(@RequestBody Payment paymentToCreate){
+      return paymentService.savePayment(paymentToCreate);
    }
 
    @RequestMapping(method = RequestMethod.GET)
    public ResponseEntity<?> getPays(){
-      List<Payment> allPays = payRepository.getAll();
-      return new ResponseEntity<>(allPays, HttpStatus.OK);
+      List<Payment> allPayments = paymentService.getAllPayments();
+      return new ResponseEntity<>(allPayments, HttpStatus.OK);
    }
 
    @ExceptionHandler({Exception.class})
