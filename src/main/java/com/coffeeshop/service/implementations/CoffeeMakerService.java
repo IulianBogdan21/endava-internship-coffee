5de8eba9cd9db901d4a536abc84ff3f46f91bc76
Bogdan Iulian <vali_0894@yahoo.com>
2022-08-06 13:48:59 +0300

    [US#5] Remove CoffeeMakerRepository

--- src/main/java/com/coffeeshop/service/implementations/CoffeeMakerService.java
+++ src/main/java/com/coffeeshop/service/implementations/CoffeeMakerService.java
@@ -1,10 +1,9 @@
 package com.coffeeshop.service.implementations;
 
 import com.coffeeshop.models.coffeeRoot.Coffee;
-import com.coffeeshop.repository.implementations.CoffeeMakerRepository;
 import com.coffeeshop.service.interfaces.ICoffeeMakerService;
 import com.coffeeshop.models.shop.Ingredients;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.coffeeshop.utilitary.managers.CoffeeManager;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -12,17 +11,10 @@ import java.util.Map;
 @Service("coffeeMakerService")
 public class CoffeeMakerService implements ICoffeeMakerService {
 
-    private CoffeeMakerRepository coffeeMakerRepository;
-
     public CoffeeMakerService(){}
-
-    @Autowired
-    public CoffeeMakerService(CoffeeMakerRepository coffeeMakerRepository){
-        this.coffeeMakerRepository = coffeeMakerRepository;
-    }
 
     @Override
     public Coffee brewCoffeeAfterRecipe(Map<Ingredients, Integer> ingredients){
-        return coffeeMakerRepository.brewCoffee(ingredients);
+        return CoffeeManager.makeCoffeeFromIngredients(ingredients);
     }
 }
